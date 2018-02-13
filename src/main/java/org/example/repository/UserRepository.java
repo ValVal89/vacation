8f3a23b6b3ce02888bc358c108d4a0f820b81471
vmartinovich <mv26091989@gmail.com>
2018-02-13 18:43:18 +0300

    make Dashboard String

--- src/main/java/org/example/repository/UserRepository.java
+++ src/main/java/org/example/repository/UserRepository.java
@@ -3,9 +3,11 @@ package org.example.repository;
 import org.example.model.User;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
+@Repository
 public interface UserRepository extends MongoRepository<User, String>
 {
     public User findByFirstName(String firstName);
