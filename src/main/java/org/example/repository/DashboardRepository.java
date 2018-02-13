268608f0ffb1c963ae17e1c100f408e303686471
vmartinovich <mv26091989@gmail.com>
2018-02-13 18:39:16 +0300

    add @Repository

--- src/main/java/org/example/repository/DashboardRepository.java
+++ src/main/java/org/example/repository/DashboardRepository.java
@@ -2,9 +2,9 @@ package org.example.repository;
 
 import org.example.model.Dashboard;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
 
-import java.util.Set;
-
+@Repository
 public interface DashboardRepository extends MongoRepository<Dashboard, String> {
     Dashboard findByUser_Id(String userId);
 }
